9a123b4c33bbe9719ccaba9956692773cf653472
dtaalbers <tom@securancy.com>
2016-03-21 12:23:48 +0100

    Added show information function

---
+++ source/MassSmuggler/Routines/App/Routine.ShowHelpAndQuit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassSmuggler.Routines.App
+{
+    partial class Routine
+    {
+		public static void ShowHelpAndQuit()
+		{
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"
+ Export
+ --export           :   [required] Export database(s) 
+ --all              :   [required] Select all databases  
+ --url  <value>     :   [required] Raven DB server instance URL
+ --path <value>     :   [required] Ravendump files destination path
+
+ Other
+ --help             :   Show helping information
+                ");
+            Console.ResetColor();
+            Routines.App.Routine.Quit();
+        }
+    }
+}
